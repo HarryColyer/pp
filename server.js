54e11fb5e8188be6d2c0cfc523fd5ce9af9ac4a3
Harryc <harrycolyer@hotmail.co.uk>
2023-09-30 20:11:13 +0100

    again

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ app.post("/api/contact", async (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '127.0.0.1', () => {
   console.log(`Server running on port ${PORT}`);
 });
