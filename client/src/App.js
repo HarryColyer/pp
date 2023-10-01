3aa62415c0836b60a830a8ffcb5444960cb5a92a
Harryc <harrycolyer@hotmail.co.uk>
2023-10-01 14:33:29 +0100

    browserrouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouterRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
 import Home from "./pages/Home";
 import About from "./pages/About";
